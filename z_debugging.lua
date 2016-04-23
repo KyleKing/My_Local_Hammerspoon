9675ea2bd4a6faea16d082b691a479256618e6a2
Kyle King <kmking72@gmail.com>
2016-04-23 16:48:13 -0400

    init

---
+++ z_debugging.lua
@@ -0,0 +1,5 @@
+-- --
+-- -- Create logging tool for debugging
+-- --
+-- local log = hs.logger.new('mymodule','debug')
+-- log.i('Initializing') -- will print "[mymodule] Initializing" to the console
