78dca6c583333aac6062b6bc5d62584003a4ef9f
Kyle King <kmking72@gmail.com>
2016-05-23 23:01:26 -0400

    Improved sound: streamkeys and headphone watcher

--- Mac_Sound.lua
+++ Mac_Sound.lua
@@ -31,10 +31,21 @@ function spotify_track()
 end
 
 -- Mute ads
+function streamkeys_previous()
+  hs.eventtap.keyStroke({"ctrl"}, "j")
+end
+function streamkeys_playpause()
+  hs.eventtap.keyStroke({"ctrl"}, "k")
+end
+function streamkeys_next()
+  hs.eventtap.keyStroke({"ctrl"}, "l")
+end
+
+-- Mute ads
 function mute_ads()
   local artist = hs.spotify.getCurrentArtist()
   if Utility.isEmpty(artist) then
-    hs.audiodevice.defaultOutputDevice():setVolume(0)
+    -- hs.audiodevice.defaultOutputDevice():setVolume(0)
     hs.audiodevice.defaultOutputDevice():setMuted(true)
     -- hs.alert.show('MUTING')
     mute_timer = hs.timer.doAfter(5, function() mute_ads() end)
@@ -42,7 +53,7 @@ function mute_ads()
     if not type(volume_prev) == 'number' then
       volume_prev = 20
     end
-    hs.audiodevice.defaultOutputDevice():setVolume(volume_prev)
+    -- hs.audiodevice.defaultOutputDevice():setVolume(volume_prev)
     hs.audiodevice.defaultOutputDevice():setMuted(false)
     hs.alert.show('UN-muting')
   end
@@ -50,27 +61,58 @@ end
 
 -- -- Demonstration of passing a function as an argument
 -- Note: do not include the () of the function
-function checkSpotify( func )
+function checkSpotify( func, funcAlt )
   if hs.spotify.isRunning() then
     func()
     show_track_timer = hs.timer.doAfter(1, function() spotify_track() end)
   else
-    hs.alert.show('Spotify is not open')
+    funcAlt()
   end
 end
 
 -- If iTunes or Chrome (Streamkeys) are open, the play pause buttons
 -- can conflict. Force Spotify to play using a set of override keys
 hs.hotkey.bind(Utility.mash, 'b', function ()
-  checkSpotify(hs.spotify.previous)
+  checkSpotify(hs.spotify.previous, streamkeys_previous)
 end)
 hs.hotkey.bind(Utility.mash, 'n', function ()
-  checkSpotify(hs.spotify.playpause)
+  checkSpotify(hs.spotify.playpause, streamkeys_playpause)
 end)
 hs.hotkey.bind(Utility.mash, 'm', function ()
-  checkSpotify(hs.spotify.next)
+  checkSpotify(hs.spotify.next, streamkeys_next)
 end)
 -- Custom display track/artist:
 hs.hotkey.bind(Utility.mash, "j", function ()
-  spotify_track()
+  checkSpotify(spotify_track, streamkeysPlaypause)
 end)
+
+--------------------------------------------------
+-- Headphone Connection Watcher
+--------------------------------------------------
+
+audLog = hs.logger.new('audWatcher')
+-- audLog.setLogLevel(5) -- [0,5]
+
+-- Per-device watcher to detect headphones in/out
+function audioDeviceWatch(dev_uid, event_name, event_scope, event_element)
+  audLog.df("AudioDeviceWatch args: %s, %s, %s, %s", dev_uid, event_name, event_scope, event_element)
+  aud = hs.audiodevice.findDeviceByUID(dev_uid)
+  if event_name == 'jack' then
+    if aud:jackConnected() then
+      hs.alert.show("Auxiliary Device Connected")
+      hs.audiodevice.defaultOutputDevice():setMuted(false)
+      if aud:outputVolume() >= 40 then
+        hs.audiodevice.defaultOutputDevice():setVolume(30)
+      end
+    else
+      -- hs.alert.show("Auxiliary Device Disconnected")
+      hs.audiodevice.defaultOutputDevice():setMuted(true)
+    end
+  end
+end
+
+-- Account for all audio devices (boom is second, but also bluetooth, airplay, etc.)
+for i,aud in ipairs(hs.audiodevice.allOutputDevices()) do
+  audwatch = aud:watcherCallback(audioDeviceWatch)
+  audwatch:watcherStart()
+end
