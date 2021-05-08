7333fd4618f1865d34bbe88639a1c228ffbf2e50
Kyle King <KyleKing@users.noreply.github.com>
2021-05-08 08:10:11 -0400

    refactor: remove dead code

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # My Hammerspoon Config
 
-Hammerspoon is a great utility to simplify really mundane, but useful things. For example, I have multiple applications (iTunes, Spotify, and Streamkeys - for chrome) that expect input using the <kbd>f7</kbd> (Rewind), <kbd>f8</kbd> (Play/Pause), and <kbd>f9</kbd> (Fast forward) keys. Usually Spotify is the lowest in this hierarchy, so I would have to manually change songs; however with Hammerspoon, there is a really easy alternative to map the commands to a second set of shortcut keys -> so I wrote the song controller in `Mac_Sound.lua`.
-
-I heavily use Alfred to trigger many of the Hammerspoon/applescript functions and can pass arguments through my custom workflow. The workflow will be released in the next month, but the devlopment version is available here: https://github.com/KyleKing/My-Programming-Sketchbook/tree/master/Alfred/user.workflow.D67DE9BE-47D0-4727-BF34-DFA7132EDCD1
-
-## Rundown of the included Files
-
-- ```HelloWorld.lua```: My notes as I followed the getting started guide
-- ```Mac_*.lua```: Useful utilities for battery watching, spotify, and dot files, with more to come
-- ```Utility.lua```: Set of functions used across this app, such as printing proper JSON
-	- ```dkjson.lua```: module used within ```Utility.lua```
-- ```init.lua```: An index file that triggers successive files to be run and includes the AlfredFunction()
-- ```windowTiling.lua```: The most essential of all the shortcuts
-- ```z_In Progress.lua```: Mostly notes and functions that may work!
-- ```z_debugging.lua```: Playing around with some debugging utilities, but haven't really used
+Legacy Hammerspoon Utilities. Not actively developed, but still in use locally
--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- TODO: https://tomdebruijn.com/posts/super-fast-application-switching/
--- TODO: http://bezhermoso.github.io/2016/01/20/making-perfect-ramen-lua-os-x-automation-with-hammerspoon/
-
 print('')
 print('>> STARTED LOADING HS Profile')
 print('')
@@ -30,45 +27,6 @@ dofile("./Modules/Mac_Software.lua")
 dofile("./Modules/Mac_Sound.lua")
 local Mac = require("./Modules/MacUtilities")
 local WIP = require("./Other/z_In Progress")
-
--- ----------------------------------------------------
--- -- Any Bar (Update 11Mar2019 - disabled)
--- ----------------------------------------------------
-
--- -- -- Get current color of AnyBar
--- -- -- Wanted to see if open, but opens up the app anyway
--- -- local success, color, raw = hs.applescript([[
--- -- 	tell application "AnyBar" to set current to get image name as Unicode text
--- -- 	return current
--- -- ]])
-
--- -- -- Previous write to file approach:
--- -- -- local tContents = Utility.read_file(Utility.file, 'l')
--- -- -- if tContents[6] == 'false' then
-
--- -- Open AnyBar, by checking for active processes:
--- local result = Utility.captureNEW("ps aux | grep -i '[a]nybar'")
--- if result == '' then
--- 	os.execute('open /Users/kyleking/Applications/AnyBar.app')
--- 	os.execute('ANYBAR_PORT='..Utility.anybar1..' open -na AnyBar')
--- 	os.execute('ANYBAR_PORT='..Utility.anybar2..' open -na AnyBar')
--- 	os.execute('ANYBAR_PORT='..Utility.anybar3..' open -na AnyBar')
--- 	-- -- Utility.change_file_line(Utility.file, 6, true)
--- else
--- 	Utility.AnyBarUpdate( "black", true )
--- 	-- -- Utility.change_file_line(Utility.file, 6, false)
--- end
--- -- Check Do Not Disturb Status:
--- local tContents = Utility.read_file(Utility.file, 'l')
--- if tContents[8] == 'on' then
---   Utility.AnyBarUpdate( "exclamation", Utility.anybar3 )
--- else
---   Utility.AnyBarUpdate( "black", Utility.anybar3 )
--- end
-
--- -- Kill AnyBar if needed:
--- -- -- Setup sudoers: https://github.com/Hammerspoon/hammerspoon/issues/707#issuecomment-168329103
--- -- os.execute("sudo kill $(ps aux | grep -i '[a]nybar' | awk '{print $2}')")
 
 ----------------------------------------------------
 -- Custom Alfred Triggers
@@ -146,58 +104,65 @@ function AlfredFunctions()
 		}
 	};
 
-	--[[
-     Generate additional search keys based on JSON file
-		-- Download png image: https://stackoverflow.com/a/29654933/3219667
-		-- Better icon api: http://icons.better-idea.org
-	--]]
+	-- Deactivated links.json. Use browser bookmarks instead
 
-	function create_fn(dir, fn, filetype)
-	  return dir..'favicons/'..fn..filetype
-	end
+	-- --[[
+    --  Generate additional search keys based on JSON file
+	-- 	-- Download png image: https://stackoverflow.com/a/29654933/3219667
+	-- 	-- Better icon api: http://icons.better-idea.org
+	-- If http://icons.better-idea.org/ is still offline...
+	--    http://imgorigin.icons8.com
+	-- 	  https://www.google.com/s2/favicons?domain=DOMAINLINKEHERE
+	-- Alternative: https://github.com/mat/besticon
+	--    example: https://besticon-demo.herokuapp.com/allicons.json?url=github.com
+	-- --]]
 
-	local links = Utility.readAll("./links.json")
-	local links_obj = Utility.readJSON(links)
-	local alfred_dir = '/Users/kyleking/Developer/My-Programming-Sketchbook/Alfred/user.workflow.D67DE9BE-47D0-4727-BF34-DFA7132EDCD1/'
-	for func_name,link in pairs(links_obj) do
-		-- Create filename based on JSON key
-		local fn = string.gsub(func_name, "%s+", "_")
-		local saved_fn = create_fn(dir, fn, '.png')
-		local full_fn = alfred_dir..saved_fn
-		-- Also check if the file was saved as a jpg
-		local saved_fn_jpg = create_fn(dir, fn, '.jpg')
-		local full_fn_jpg = alfred_dir..saved_fn_jpg
-		-- Check if file is already downloaded
-		if not hs.fs.attributes(full_fn) and not hs.fs.attributes(full_fn_jpg) then
-			-- Request and download image:
-			local url = 'https://icons.better-idea.org/icon?url='..link..'&size=15..300..500'
-			local code,body,headers = hs.http.doRequest(url, 'GET')
-			if body then
-				-- Troubleshoot image type
-				-- if not string.find(headers['Content-Type'], "png") then
-				-- 	print('Error: header is not type png')
-				-- 	for key,value in pairs(headers) do
-				-- 		print(key..'  '..value)
-				-- 	end
-				-- 	saved_fn = saved_fn_jpg
-				-- 	full_fn = full_fn_jpg
-				-- end
-				local f = assert(io.open(full_fn, 'wb'))
-				f:write(body)
-				f:close()
-			else
-				-- Set to the fallback favicon
-				saved_fn = create_fn(dir, 'unknown', '.png')
-				-- print(string.format('\nError: icons.better-idea.org failed to return an icon for: "%s"', url))
-			end
-		end
-		-- Update object called by Alfred:
-		table.insert(sometable, {
-				["func_name"]="l "..func_name,
-				["description"]="Link to: "..link,
-				["icon"]=saved_fn
-			})
-	end
+	-- function create_fn(dir, fn, filetype)
+	--   return dir..'favicons/'..fn..filetype
+	-- end
+
+	-- local links = Utility.readAll("./links.json")
+	-- local links_obj = Utility.readJSON(links)
+	-- local alfred_dir = '/Users/kyleking/Developer/My-Programming-Sketchbook/Alfred/user.workflow.D67DE9BE-47D0-4727-BF34-DFA7132EDCD1/'
+	-- for func_name,link in pairs(links_obj) do
+	-- 	-- Create filename based on JSON key
+	-- 	local fn = string.gsub(func_name, "%s+", "_")
+	-- 	local saved_fn = create_fn(dir, fn, '.png')
+	-- 	local full_fn = alfred_dir..saved_fn
+	-- 	-- Also check if the file was saved as a jpg
+	-- 	local saved_fn_jpg = create_fn(dir, fn, '.jpg')
+	-- 	local full_fn_jpg = alfred_dir..saved_fn_jpg
+	-- 	-- Check if file is already downloaded
+	-- 	if not hs.fs.attributes(full_fn) and not hs.fs.attributes(full_fn_jpg) then
+	-- 		-- Request and download image:
+	-- 		local url = 'https://icons.better-idea.org/icon?url='..link..'&size=15..300..500'
+	-- 		local code,body,headers = hs.http.doRequest(url, 'GET')
+	-- 		if body then
+	-- 			-- Troubleshoot image type
+	-- 			-- if not string.find(headers['Content-Type'], "png") then
+	-- 			-- 	print('Error: header is not type png')
+	-- 			-- 	for key,value in pairs(headers) do
+	-- 			-- 		print(key..'  '..value)
+	-- 			-- 	end
+	-- 			-- 	saved_fn = saved_fn_jpg
+	-- 			-- 	full_fn = full_fn_jpg
+	-- 			-- end
+	-- 			local f = assert(io.open(full_fn, 'wb'))
+	-- 			f:write(body)
+	-- 			f:close()
+	-- 		else
+	-- 			-- Set to the fallback favicon
+	-- 			saved_fn = create_fn(dir, 'unknown', '.png')
+	-- 			-- print(string.format('\nError: icons.better-idea.org failed to return an icon for: "%s"', url))
+	-- 		end
+	-- 	end
+	-- 	-- Update object called by Alfred:
+	-- 	table.insert(sometable, {
+	-- 			["func_name"]="l "..func_name,
+	-- 			["description"]="Link to: "..link,
+	-- 			["icon"]=saved_fn
+	-- 		})
+	-- end
 
 	Utility.printJSON(sometable)
 end
